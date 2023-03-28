99ace0275442715c8b0d3a4ce36142e2d2300ad0
ishsraibit <nac82_220419@gclass.ice.go.kr>
2023-03-28 19:19:04 +0900

    week02 TemperatueConverter03

--- week 03/src/TemperaturejavaConverterDemo.java
+++ week 03/src/TemperaturejavaConverterDemo.java
@@ -7,28 +7,26 @@ public class TemperaturejavaConverterDemo {
         double fahrenheit = 0.0 ;
         double celsius = 0.0;
 
-        char c = '\uB208';
-        //c = '나';
-        System.out.println(c);
-
         Scanner scanner = new Scanner(System.in);
         //1) Fahrenheit To Celsius 2)Celsius To Fahrenheit 3)Exit ;
         while (true){
             System.out.print("1) Fahrenheit To Celsius 2)Celsius To Fahrenheit 3)Exit");
-            int menu = scanner.nextInt();
-            if(menu == 1){
+            String menu = scanner.nextLine();
+            //if(menu.eq "1"){
+            if(menu.equals("1")
+        ){
                 System.out.print("Enter temperature in Fahrenhiet : ");
                 fahrenheit = scanner.nextDouble();
                 celsius = (fahrenheit-32.0) * (5.0/9.0);
                 System.out.println(fahrenheit+"degrees Rahrenheit is "+celsius+" degrees Celsius");
-            } else if(menu == 2){
-                //for you (°C × 9/5) + 32 = °F
+            } else if(menu.equals("2")){
+                //for you 2(°C × 9/5) + 32 = °F
                 System.out.print("Enter temperature in Celsius : ");
-                fahrenheit = (celsius * 9.0/5.0)+32.0;
+                fahrenheit = (celsius * (9.0/5.0)) + 32.0;
                 celsius = scanner.nextDouble();
                 System.out.println(celsius+" degrees Celsius is "+fahrenheit+"degrees Rahrenheit ");
 
-            } else if(menu == 3){
+            } else if(menu.equals("3")){
                 System.out.println("Program finishedd!");
                 break;
             }else{
