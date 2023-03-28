d027b6cc14f1ec2767bbb2da37e99ad78242a985
ishsraibit <nac82_220419@gclass.ice.go.kr>
2023-03-28 19:36:31 +0900

    week03  TemperatureConveter03

--- week 03/src/TemperaturejavaConverterDemo.java
+++ week 03/src/TemperaturejavaConverterDemo.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class TemperaturejavaConverterDemo {
@@ -12,25 +13,30 @@ public class TemperaturejavaConverterDemo {
         while (true){
             System.out.print("1) Fahrenheit To Celsius 2)Celsius To Fahrenheit 3)Exit");
             String menu = scanner.nextLine();
-            //if(menu.eq "1"){
-            if(menu.equals("1")
-        ){
-                System.out.print("Enter temperature in Fahrenhiet : ");
-                fahrenheit = scanner.nextDouble();
-                celsius = (fahrenheit-32.0) * (5.0/9.0);
-                System.out.println(fahrenheit+"degrees Rahrenheit is "+celsius+" degrees Celsius");
-            } else if(menu.equals("2")){
-                //for you 2(°C × 9/5) + 32 = °F
-                System.out.print("Enter temperature in Celsius : ");
-                fahrenheit = (celsius * (9.0/5.0)) + 32.0;
-                celsius = scanner.nextDouble();
-                System.out.println(celsius+" degrees Celsius is "+fahrenheit+"degrees Rahrenheit ");
-
-            } else if(menu.equals("3")){
-                System.out.println("Program finishedd!");
+            if (menu.equals("3")){
+                System.out.println("Program Finished!");
                 break;
-            }else{
-                System.out.print("Please choose from the given menu : ");
+            }
+            //if(menu.eq "1")
+            switch (menu){
+                case "1":
+
+                    System.out.print("Enter temperature in Fahrenhiet : ");
+                    fahrenheit = scanner.nextDouble();
+                    celsius = (fahrenheit-32.0) * (5.0/9.0);
+                    System.out.println(fahrenheit+"degrees Rahrenheit is "+celsius+" degrees Celsius");
+                    break;
+                case "2":
+                //for you 2(°C × 9/5) + 32 = °F
+                    System.out.print("Enter temperature in Celsius : ");
+                    fahrenheit = (celsius * (9.0/5.0)) + 32.0;
+                    celsius = scanner.nextDouble();
+                    System.out.println(celsius+" degrees Celsius is "+fahrenheit+"degrees Rahrenheit ");
+                    break;
+
+                default:
+                    System.out.print("Please choose from the given menu : ");
+                    break;
             }
         }
     }
