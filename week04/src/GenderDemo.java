b8fc86cfad4df8eeb9fbff9acc4eacdbe2890620
ishsraibit <nac82_220419@gclass.ice.go.kr>
2023-03-28 19:55:38 +0900

    week04 switch v0.4

--- week04/src/GenderDemo.java
+++ week04/src/GenderDemo.java
@@ -10,18 +10,12 @@ public class GenderDemo {
         char gender = socialSecurityNumber.charAt(6);
 //        System.out.println(socialSecurityNumber);
         //System.out.println(gender);
-        switch (gender){
-            case '1':
-            case '3':
-                System.out.println("Male");
-                break;
-            case '2':
-            case '4':
-                System.out.println("Female");
-                break;
-            default:
-                System.out.println("Wrong ssn!");1
-        }
+        String genderstring =  switch (gender){
+            case '1', '3' -> "Male";
+            case '2', '4' -> "Female";
+            default -> "Wrong ssn!";
+        };
+        System.out.println(genderstring);
 
     }
 }
