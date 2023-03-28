6afcbfa7f1843f5c1a3051fd40c529070f0db1e4
ishsraibit <nac82_220419@gclass.ice.go.kr>
2023-03-28 21:48:34 +0900

    week04 main method args v0.2

--- week04/src/MainDemo.java
+++ week04/src/MainDemo.java
@@ -1,16 +1,24 @@
 public class MainDemo {
     //javac MainDemo.java-encoding utf-8
-    //java MainDemo 7 9
-    //16
+    //java MainDemo 0604074123456
 
-    //or
+    //Male
+
     //InteliJidea
     //Run -> Edit Configurations -> Build and run -> Program arguments
 
     public static void main(String[] args) {
-        System.out.println("length of array : "+ args.length);
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        System.out.println(a+b);
+        String socialSecurityNumber = args[0];
+        char gender = socialSecurityNumber.charAt(6);
+//        System.out.println(socialSecurityNumber);
+        //System.out.println(gender);
+        String genderstring =  switch (gender){
+            case '1', '3' -> "Male";
+            case '2', '4' -> "Female";
+            default -> "Wrong ssn!";
+        };
+        System.out.println(genderstring);
+
+
     }
 }
