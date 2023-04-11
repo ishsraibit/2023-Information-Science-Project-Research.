edc52b8a5e59c850515fb6cbe4e64b64ccef04dc
ishsraibit <nac82_220419@gclass.ice.go.kr>
2023-04-11 19:42:13 +0900

    week06 class & object v0.2

--- week06/src/game/Pocketmon.java
+++ week06/src/game/Pocketmon.java
@@ -1,16 +1,41 @@
 package game;
 
 public class Pocketmon {
-    String name;
-    int level;
+    //encapsulation
+    private String name;
+    private int level;
+    private int hp;
 
-    int hp;
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public String getName(){
+        return name;
+
+    }
+
+    public void setName(String name){
+        this.name = name;
+        //name = n;
+    }
     //methods
     void evolve(){
         level = level+1;
         hp = hp + 10;
-        //System.out.println(""+ level + "로 성장합니다.");
-        System.out.println(level + "레벨로 성장합니다.");
+        System.out.println( level + "레벨로 성장합니다.");
 
     }
     void attack(){
--- week06/src/game/pocketmongame.java
+++ week06/src/game/pocketmongame.java
@@ -5,14 +5,18 @@ public class pocketmongame {
         Pocketmon pikachu = new Pocketmon();
         Pocketmon charizard = new Pocketmon();
 
-        pikachu.name = "피카츄";
-        charizard.name = "리자몽";
-        pikachu.level = 5;
-        charizard.level = 36;
+        pikachu.setName("피카츄");
+        charizard.setName("리자몽");
 
-        System.out.print(pikachu.name+"가 ");
+        System.out.println(pikachu.getName());
+        pikachu.setLevel(5);
+        charizard.setLevel(36);
+
+
         pikachu.evolve();
         pikachu.attack();
         charizard.evolve();
+
+        System.out.println(pikachu.getLevel());
     }
 }
