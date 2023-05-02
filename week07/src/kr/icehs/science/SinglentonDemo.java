49f2c84e9617ee795b3d375bac6c0ef3dfc84f86
ishsraibit <nac82_220419@gclass.ice.go.kr>
2023-05-02 20:24:47 +0900

    week07 class, singleton pattern v0.2

--- week07/src/kr/icehs/science/SinglentonDemo.java
+++ week07/src/kr/icehs/science/SinglentonDemo.java
@@ -1,13 +1,19 @@
 package kr.icehs.science;
 
 class Singleton{
+    private static Singleton s = new Singleton(); //unique object
     private Singleton() {
-        System.out.println("단 하나의 객체!");
+        System.out.println("One Object Created!");
+    }
+
+    public static Singleton getInstance() {
+        return s;
     }
 }
 public class SinglentonDemo {
     public static void main(String[] args) {
         //Singleton s = new Singleton();  // private acess
+        System.out.println(Singleton.getInstance().toString());
     }
 
 }
